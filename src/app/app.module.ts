0dce3082a8e94203b8dc068e7c5cd4b4e45b97a1
Lucas França Marinho <lucasfrancamarinho16@gmail.com>
2023-02-01 00:02:00 -0300

    feat: add Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,12 @@ import { FuncionarioComponent } from './home/funcionario/funcionario.component';
 import { VeterinarioComponent } from './login/veterinario/veterinario.component';
 import { ListaClienteComponent } from './login/atendente/lista-cliente/lista-cliente.component';
 import { SidenavComponent } from './login/atendente/sidenav/sidenav.component'
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from 'src/environments/environment';
+
+import { provideAuth, getAuth } from '@angular/fire/auth'
+import { provideFirestore, getFirestore } from '@angular/fire/firestore'
+import { provideStorage, getStorage } from '@angular/fire/storage'
 
 @NgModule({
   declarations: [
@@ -32,6 +38,10 @@ import { SidenavComponent } from './login/atendente/sidenav/sidenav.component'
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: false,
-
+  firebase: {
+  apiKey: "AIzaSyDp6UVL_BEmpoOTikse3hLmkMz12AtUWC0",
+  authDomain: "petmania-ec6c4.firebaseapp.com",
+  projectId: "petmania-ec6c4",
+  storageBucket: "petmania-ec6c4.appspot.com",
+  messagingSenderId: "1083505789214",
+  appId: "1:1083505789214:web:6d42750c0eda9e9403a10f"}
 };
