fa872cbba46dc33333ff8041f6dba2a1ed32b208
Lucas França Marinho <lucasfrancamarinho16@gmail.com>
2023-02-08 15:28:47 -0300

    feat: Logout icon Vet-area

--- src/app/login/veterinario/sidenav-vet/sidenav-vet.component.ts
+++ src/app/login/veterinario/sidenav-vet/sidenav-vet.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sidenav-vet',
@@ -10,9 +11,14 @@ export class SidenavVetComponent {
   isActive = false;
 
   constructor(private router: Router,
-
+    private authService: AuthService
     ){
 
+  }
+
+  async logout() {
+    await this.authService.logout();
+    this.router.navigateByUrl('/', { replaceUrl: true });
   }
 
   routerListaFicha(){
