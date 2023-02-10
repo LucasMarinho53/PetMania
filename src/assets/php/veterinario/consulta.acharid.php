2290fd89001c7245733b05713b6b6e18070bfb48
Rafael Moura <rafael.g.moura@hotmail.com>
2023-02-10 20:58:25 -0300

    update: cors

--- src/assets/php/veterinario/consulta.acharid.php
+++ src/assets/php/veterinario/consulta.acharid.php
@@ -1,5 +1,11 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header("content-type: application/json");
+//header("Content-type: application/x-www-form-urlencoded");
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: *");
+
 require '../crud/veterinario.crud.php';
 
 if(isset($_GET['buscar']) && !empty($_GET['buscar']) && $_GET['buscar'] != NULL)
--- src/assets/php/veterinario/consulta.atualizar.php
+++ src/assets/php/veterinario/consulta.atualizar.php
@@ -1,5 +1,11 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header("content-type: application/json");
+//header("Content-type: application/x-www-form-urlencoded");
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: *");
+
 require '../crud/veterinario.crud.php';
 
 $_POST = json_decode(file_get_contents('php://input'), true);
