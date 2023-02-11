766b62368858a5e7946546317c1f488e8ddbfadb
Rafael Moura <rafael.g.moura@hotmail.com>
2023-02-11 10:46:19 -0300

    update:integracao-cadastro

--- src/assets/php/atendente/cliente/cliente.cadastro.php
+++ src/assets/php/atendente/cliente/cliente.cadastro.php
@@ -7,21 +7,22 @@ header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: *");
 
 require('../../crud/atendente.crud.php');
-
+ini_set('display_errors', 0);
 $_POST = json_decode(file_get_contents('php://input'), true);
-
+//echo json_encode($_POST,JSON_UNESCAPED_UNICODE);
 
 if($_POST['nome'] == NULL ||
    $_POST['cpf'] == NULL ||
    $_POST['email'] == NULL ||
    $_POST['telefone'] == NULL ||
-   $_POST['localidade'] == NULL ||
+   $_POST['cidade'] == NULL ||
    $_POST['bairro'] == NULL ||
    $_POST['logradouro'] == NULL ||
    $_POST['cep'] == NULL ||
    $_POST['numero'] == NULL)
 {
     echo "{\"result\":\"incomplete data\"}";
+    //echo "{\"result\":".json_encode($__POST)."}";
     die();
 }
 
@@ -30,7 +31,7 @@ $cliente->cpf = $_POST['cpf'];
 $cliente->nome = $_POST['nome'];
 $cliente->email = $_POST['email'];
 $cliente->telefone = $_POST['telefone'];
-$cliente->cidade = $_POST['localidade'];
+$cliente->cidade = $_POST['cidade'];
 $cliente->bairro = $_POST['bairro'];
 $cliente->logradouro = $_POST['logradouro'];
 $cliente->numero = $_POST['numero'];
