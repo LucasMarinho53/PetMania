276dc1691befa9536cb28d189d0126dd23f7413c
Isaque-Fernandes <isaquefernandesuser@gmail.com>
2023-02-13 15:16:10 -0300

    feat: getConsulta and getUserById concluded

--- src/app/prontuario/prontuario.page.ts
+++ src/app/prontuario/prontuario.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../services/auth.service';
 import { Consulta } from '../models/Consulta.model';
 import { Ficha } from '../models/Ficha.model';
 import { FirebaseService } from '../services/firebase.service';
@@ -10,17 +11,30 @@ import { FirebaseService } from '../services/firebase.service';
   styleUrls: ['./prontuario.page.scss'],
 })
 export class ProntuarioPage implements OnInit {
+  fichaUsuarioLogado!: Ficha;
   consultas!: Consulta[];
-  fichas!: Ficha[]
+  fichas!: Ficha[];
 
-  constructor(private firebaseService: FirebaseService) {}
+  constructor(
+    private firebaseService: FirebaseService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    let email = this.authService.userId();
+    if (email) {
+      this.authService.encontrarPorId(email).subscribe((fichaUsuarioLogado) => {
+        this.fichaUsuarioLogado = fichaUsuarioLogado;
+        // console.log(this.fichaUsuarioLogado);
+      });
+    }
+
     this.firebaseService.getConsulta().subscribe({
-      next: (res) => {
-        // console.log(res[1].consultas[0].nome_dono)
-        // console.log(res[1].consultas)
-        this.fichas = res;
+      next: (todasFichas) => {
+        this.fichas = todasFichas.filter(
+          (ficha) => ficha.id_ficha === this.fichaUsuarioLogado.id_ficha
+        );
+        // console.log(this.fichas);
       },
       error: (e) => {
         console.error(e);
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,49 +1,86 @@
-import { Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
-import { Dono } from '../models/dono.model';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+} from '@angular/fire/auth';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  doc,
+  docSnapshots,
+  setDoc,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { Observable, map } from 'rxjs';
 
+import { Dono } from '../models/dono.model';
+import { Ficha } from '../models/Ficha.model';
+import { Injectable } from '@angular/core';
+
+const CONSULTA = 'consulta';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private auth: Auth) { }
+  constructor(private auth: Auth, private firestore: Firestore) {}
 
   async register(dono: Dono) {
-    try{
-    const user = await createUserWithEmailAndPassword(
-      this.auth,
-      dono.email,
-      dono.senha
-       );
-       return user;
+    try {
+      const user = await createUserWithEmailAndPassword(
+        this.auth,
+        dono.email,
+        dono.senha
+      );
+      return user;
     } catch (e) {
       return null;
     }
   }
 
-
-  async login (dono: Dono) {
-      try{
-        const user = await signInWithEmailAndPassword(
-      this.auth,
-      dono.email,
-      dono.senha
-       );
-       return user;
+  async login(dono: Dono) {
+    try {
+      const user = await signInWithEmailAndPassword(
+        this.auth,
+        dono.email,
+        dono.senha
+      );
+      return user;
     } catch (e) {
       return null;
     }
   }
 
-  async senhaperdida(email:string){
-    const mail = sendPasswordResetEmail(this.auth,email);
+  async senhaperdida(email: string) {
+    const mail = sendPasswordResetEmail(this.auth, email);
     return mail;
-
   }
 
   logout() {
     return signOut(this.auth);
+  }
+
+  encontrarPorId(id_ficha: string): Observable<Ficha> {
+    const document = doc(this.firestore, `${CONSULTA}/${id_ficha}`);
+
+    return docSnapshots(document).pipe(
+      map((doc) => {
+        const id_ficha = doc.id;
+        const data = doc.data();
+
+        return { id_ficha, ...data } as Ficha;
+      })
+    );
+  }
+
+  userId() {
+    if (this.auth.currentUser) {
+      return this.auth.currentUser!.email;
+    } else {
+      return;
+    }
   }
 }
